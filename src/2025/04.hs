02e81efb7cfcc04ba897baf4193bf2bc01230ab4
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-09 15:23:05 -0500

    2025 day 6

--- src/2025/04.hs
+++ src/2025/04.hs
@@ -5,24 +5,18 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ViewPatterns #-}
 
-import Control.Comonad.Store (Comonad (extract), ComonadStore, extend, pos)
-import Control.Comonad.Store.Class (peek)
+import Control.Comonad.Store (Comonad (extract), ComonadStore (experiment), extend, pos)
 import Control.Lens (FoldableWithIndex (ifoldMap'), view)
-import Data.AffineSpace (AffineSpace)
 import Data.Containers.ListUtils (nubOrd)
 import Data.Foldable (toList)
 import Data.Function (on)
 import Data.List (unfoldr)
 import Data.Monoid (getSum)
-import GHC.TypeLits (KnownNat)
+import GHC.TypeLits (KnownNat, type (<=))
 import qualified GHC.TypeLits as GHC
 import SizedGrid
 
-data Cell = Paper | Empty deriving (Eq)
-
-instance Show Cell where
-  show Paper = "@"
-  show Empty = " "
+data Cell = Paper | Empty deriving (Eq, Show)
 
 -- $setup
 -- >>> input = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@."
@@ -34,69 +28,36 @@ main = do
   print $ solve input
   print $ partTwo input
 
-charToCell :: Char -> Cell
-charToCell '@' = Paper
-charToCell _ = Empty
-
 -- >>> parse @10 input
--- Just (Grid {unGrid = [ , ,@,@, ,@,@,@,@, ,@,@,@, ,@, ,@, ,@,@,@,@,@,@,@, ,@, ,@,@,@, ,@,@,@,@, , ,@, ,@,@, ,@,@,@,@, ,@,@, ,@,@,@,@,@,@,@, ,@, ,@, ,@, ,@, ,@,@,@,@, ,@,@,@, ,@,@,@,@, ,@,@,@,@,@,@,@,@, ,@, ,@, ,@,@,@, ,@, ]})
+-- Just (Grid {unGrid = [Empty,Empty,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Empty,Paper,Paper,Paper,Empty,Paper,Empty,Paper,Empty,Paper,Paper,Paper,Paper,Paper,Paper,Paper,Empty,Paper,Empty,Paper,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Empty,Empty,Paper,Empty,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Empty,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Paper,Paper,Paper,Empty,Paper,Empty,Paper,Empty,Paper,Empty,Paper,Empty,Paper,Paper,Paper,Paper,Empty,Paper,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Empty,Paper,Paper,Paper,Paper,Paper,Paper,Paper,Paper,Empty,Paper,Empty,Paper,Empty,Paper,Paper,Paper,Empty,Paper,Empty]})
 parse :: (KnownNat n, KnownNat (n GHC.* n)) => String -> Maybe (Grid '[HardWrap n, HardWrap n] Cell)
 parse = gridFromList . map (map charToCell) . lines
+  where
+    charToCell '@' = Paper
+    charToCell _ = Empty
 
 -- >>> solve example
--- 13
-solve ::
-  ( AllSizedKnown cs,
-    All IsCoordLifted cs,
-    All AffineSpace cs,
-    AllDiffSame Integer cs,
-    All Eq cs,
-    All Monoid cs,
-    All Semigroup cs,
-    All Ord cs
-  ) =>
-  FocusedGrid cs Cell ->
-  Int
+-- 71
+solve, partTwo :: (KnownNat n, 1 <= n) => FocusedGrid '[HardWrap n, HardWrap n] Cell -> Int
 solve g = getSum $ ifoldMap' go (view asGrid g)
   where
     go coord = \case
-      Paper | isAccessible g coord -> 1
+      Paper | forklift g coord -> 1
       _ -> 0
-
-isAccessible ::
-  ( AllDiffSame Integer cs,
-    All Eq cs,
-    All Ord cs,
-    All AffineSpace cs,
-    ComonadStore (Coord cs) w
-  ) =>
-  w Cell ->
-  Coord cs ->
-  Bool
-isAccessible g coord =
-  let neighs = filter (/= coord) (nubOrd $ moorePoints (1 :: Integer) coord)
-      papers = length $ filter (\p -> peek p g == Paper) neighs
-   in papers < 4
 
 -- >>> partTwo example
 -- 43
-partTwo ::
-  ( AllSizedKnown cs,
-    All Ord cs,
-    All Semigroup cs,
-    All Monoid cs,
-    All Eq cs,
-    All AffineSpace cs,
-    All IsCoordLifted cs,
-    AllDiffSame Integer cs
-  ) =>
-  FocusedGrid cs Cell ->
-  Int
 partTwo = sum . unfoldr go
   where
     go g
       | removed == 0 = Nothing
       | otherwise = Just (removed, g')
       where
-        g' = extend (\fg -> if isAccessible g (pos fg) then Empty else extract fg) g
+        g' = extend (\c -> if forklift g (pos c) then Empty else extract c) g
         removed = on (-) (length . filter (== Paper) . toList) g g'
+
+forklift :: (KnownNat n, 1 <= n) => FocusedGrid '[HardWrap n, HardWrap n] Cell -> Coord '[HardWrap n, HardWrap n] -> Bool
+forklift g coord =
+  let neighs = experiment (filter (/= coord) . nubOrd . moorePoints 1) g
+      papers = length $ filter (== Paper) neighs
+   in papers < 4
