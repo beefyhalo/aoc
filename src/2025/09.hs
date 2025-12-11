add6c4b95b48cd60ce07b10b70e925228fac3300
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-11 16:33:52 -0500

    cleanup

--- src/2025/09.hs
+++ src/2025/09.hs
@@ -2,14 +2,11 @@
 
 module Main where
 
+import Control.Arrow ((***))
 import Data.Either (partitionEithers)
 import Data.List (sortOn)
 
 type Coord = (Int, Int)
-
-type HLine = (Int, Int, Int) -- y, x_min, x_max
-
-type VLine = (Int, Int, Int) -- x, y_min, y_max
 
 -- $setup
 -- >>> input = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3"
@@ -36,24 +33,21 @@ area (x1, y1) (x2, y2) = (abs (x2 - x1) + 1) * (abs (y2 - y1) + 1)
 partTwo :: [Coord] -> Int
 partTwo poly =
   maximum
-    [ area a b | a@(ax, ay) <- poly, b@(bx, by) <- poly, checkRect (min ax bx, min ay by, max ax bx, max ay by)
+    [ area a b | a@(ax, ay) <- poly, b@(bx, by) <- poly, check (min ax bx, min ay by, max ax bx, max ay by)
     ]
   where
-    (h, v) = edges
-    hSorted = sortOn (\(y, _, _) -> y) h
-    vSorted = sortOn (\(x, _, _) -> x) v
-
     -- pair up consecutive polygon vertices into horizontal/vertical segments
-    edges :: ([HLine], [VLine])
-    edges = partitionEithers $ zipWith edge poly (tail poly ++ [head poly])
+    (hSorted, vSorted) = (sortOn fst3 *** sortOn fst3) edges
       where
         edge (ax, ay) (bx, by)
           | ax == bx = Right (ax, min ay by, max ay by)
           | otherwise = Left (ay, min ax bx, max ax bx)
+        edges = partitionEithers $ zipWith edge poly (tail poly ++ [head poly])
+        fst3 (a, _, _) = a
 
     -- rectangle bounds check
-    checkRect :: (Int, Int, Int, Int) -> Bool
-    checkRect (ax, ay, bx, by) = not $ any crossH hSlice || any crossV vSlice
+    check :: (Int, Int, Int, Int) -> Bool
+    check (ax, ay, bx, by) = not $ any crossH hSlice || any crossV vSlice
       where
         crossH (_, x1, x2) = (x1 <= ax && ax < x2) || (x1 < bx && bx <= x2)
         crossV (_, y1, y2) = (y1 <= ay && ay < y2) || (y1 < by && by <= y2)
