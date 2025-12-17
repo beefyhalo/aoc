fefd97c5df000d67e0deed3a221c98c4ad8d6b09
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-17 16:37:53 -0500

    2015 day 6 nice

--- src/2015/06.hs
+++ src/2015/06.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
@@ -12,8 +13,6 @@ import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
 import Data.Word (Word64)
 
-type Grid s = MV.MVector s Word64
-
 size, rowWords :: Int
 size = 1000
 rowWords = (size + 63) `div` 64
@@ -21,54 +20,58 @@ rowWords = (size + 63) `div` 64
 index :: Int -> Int -> (Int, Int)
 index x y = (y * rowWords + x `div` 64, x `mod` 64)
 
+type Pos = (Int, Int)
+
+data Op = On Pos Pos | Off Pos Pos | Toggle Pos Pos
+  deriving (Show)
+
 main :: IO ()
 main = do
-  input <- lines <$> readFile "input/2015/06.txt"
+  input <- map parse . lines <$> readFile "input/2015/06.txt"
   print $ solve input
   print $ partTwo input
 
--- >>> solve ["turn on 0,0 through 999,999", "turn off 499,499 through 500,500"]
+parse :: String -> Op
+parse s
+  | Just r <- stripPrefix "turn on " s = parseAs On r
+  | Just r <- stripPrefix "turn off " s = parseAs Off r
+  | Just r <- stripPrefix "toggle " s = parseAs Toggle r
+  | otherwise = error "invalid input"
+  where
+    parseAs f r = let [p1, p2] = map parseCoord $ splitOn " through " r in f p1 p2
+    parseCoord str = let (x, _ : y) = break (== ',') str in (read x, read y)
+
+-- >>> solve [On (0,0) (999,999), Off (499,499) (500,500)]
 -- 999996
-solve, partTwo :: [String] -> Int
+solve, partTwo :: [Op] -> Int
 solve ops = runST $ do
   g <- MV.replicate (size * rowWords) 0
-  for_ ops (apply g)
-  frozen <- V.freeze g
-  pure $ V.sum (V.map popCount frozen)
+  for_ ops (applyBit g)
+  V.sum . V.map popCount <$> V.freeze g
 
-apply :: Grid s -> String -> ST s ()
-apply g line
-  | Just r <- stripPrefix "turn on " line = go setBit r
-  | Just r <- stripPrefix "turn off " line = go clearBit r
-  | Just r <- stripPrefix "toggle " line = go complementBit r
+applyBit :: MV.MVector s Word64 -> Op -> ST s ()
+applyBit g = \case
+  On p1 p2 -> opBitRange setBit p1 p2
+  Off p1 p2 -> opBitRange clearBit p1 p2
+  Toggle p1 p2 -> opBitRange complementBit p1 p2
   where
-    parse s = let (x, _ : y) = break (== ',') s in (read x, read y)
-    go f s = do
-      let [(x1, y1), (x2, y2)] = map parse (splitOn " through " s)
-      for_ [y1 .. y2] $ \y ->
-        for_ [x1 .. x2] $ \x -> do
-          let (wi, bi) = index x y
-          MV.modify g (`f` bi) wi
+    opBitRange f (x1, y1) (x2, y2) =
+      for_ [y1 .. y2] $ \y -> for_ [x1 .. x2] $ \x ->
+        let (wi, bi) = index x y in MV.modify g (`f` bi) wi
 
-type Grid2 s = MV.MVector s Int
-
--- >>> partTwo ["turn on 0,0 through 999,999", "turn off 499,499 through 500,500", "toggle 0,0 through 999,999"]
+-- >>> partTwo [On (0,0) (999,999), Off (499,499) (500,500), Toggle (0,0) (999,999)]
 -- 2999996
 partTwo ops = runST $ do
   g <- MV.replicate (size * size) 0
-  for_ ops (apply2 g)
+  for_ ops (applyBright g)
   V.sum <$> V.freeze g
 
-apply2 :: Grid2 s -> String -> ST s ()
-apply2 g line
-  | Just r <- stripPrefix "turn on " line = go (+ 1) r
-  | Just r <- stripPrefix "turn off " line = go (\x -> max 0 (x - 1)) r
-  | Just r <- stripPrefix "toggle " line = go (+ 2) r
-  | otherwise = error "invalid input"
+applyBright :: MV.MVector s Int -> Op -> ST s ()
+applyBright g = \case
+  On p1 p2 -> opIntRange (+ 1) p1 p2
+  Off p1 p2 -> opIntRange (\v -> max 0 (v - 1)) p1 p2
+  Toggle p1 p2 -> opIntRange (+ 2) p1 p2
   where
-    parse s = let (x, _ : y) = break (== ',') s in (read x, read y)
-    go f s = do
-      let [(x1, y1), (x2, y2)] = map parse (splitOn " through " s)
-      for_ [y1 .. y2] $ \y ->
-        for_ [x1 .. x2] $ \x ->
-          MV.modify g f (y * size + x)
+    opIntRange f (x1, y1) (x2, y2) =
+      for_ [y1 .. y2] $ \y -> for_ [x1 .. x2] $ \x ->
+        MV.modify g f (y * size + x)
