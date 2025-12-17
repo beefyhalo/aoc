91f12b73525da791a917104e2a15ac0edf562cef
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-17 16:27:41 -0500

    2015 day 6 part2

--- src/2015/06.hs
+++ src/2015/06.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
@@ -15,8 +14,6 @@ import Data.Word (Word64)
 
 type Grid s = MV.MVector s Word64
 
-type Pos = (Int, Int)
-
 size, rowWords :: Int
 size = 1000
 rowWords = (size + 63) `div` 64
@@ -28,10 +25,11 @@ main :: IO ()
 main = do
   input <- lines <$> readFile "input/2015/06.txt"
   print $ solve input
+  print $ partTwo input
 
 -- >>> solve ["turn on 0,0 through 999,999", "turn off 499,499 through 500,500"]
 -- 999996
-solve :: [String] -> Int
+solve, partTwo :: [String] -> Int
 solve ops = runST $ do
   g <- MV.replicate (size * rowWords) 0
   for_ ops (apply g)
@@ -45,13 +43,32 @@ apply g line
   | Just r <- stripPrefix "toggle " line = go complementBit r
   where
     parse s = let (x, _ : y) = break (== ',') s in (read x, read y)
-    go f s =
-      let [p1, p2] = map parse (splitOn " through " s)
-       in opBitRange g f p1 p2
+    go f s = do
+      let [(x1, y1), (x2, y2)] = map parse (splitOn " through " s)
+      for_ [y1 .. y2] $ \y ->
+        for_ [x1 .. x2] $ \x -> do
+          let (wi, bi) = index x y
+          MV.modify g (`f` bi) wi
 
-opBitRange :: Grid s -> (Word64 -> Int -> Word64) -> Pos -> Pos -> ST s ()
-opBitRange g f (x1, y1) (x2, y2) =
-  for_ [y1 .. y2] $ \y ->
-    for_ [x1 .. x2] $ \x -> do
-      let (wi, bi) = index x y
-      MV.modify g (`f` bi) wi
+type Grid2 s = MV.MVector s Int
+
+-- >>> partTwo ["turn on 0,0 through 999,999", "turn off 499,499 through 500,500", "toggle 0,0 through 999,999"]
+-- 2999996
+partTwo ops = runST $ do
+  g <- MV.replicate (size * size) 0
+  for_ ops (apply2 g)
+  V.sum <$> V.freeze g
+
+apply2 :: Grid2 s -> String -> ST s ()
+apply2 g line
+  | Just r <- stripPrefix "turn on " line = go (+ 1) r
+  | Just r <- stripPrefix "turn off " line = go (\x -> max 0 (x - 1)) r
+  | Just r <- stripPrefix "toggle " line = go (+ 2) r
+  | otherwise = error "invalid input"
+  where
+    parse s = let (x, _ : y) = break (== ',') s in (read x, read y)
+    go f s = do
+      let [(x1, y1), (x2, y2)] = map parse (splitOn " through " s)
+      for_ [y1 .. y2] $ \y ->
+        for_ [x1 .. x2] $ \x ->
+          MV.modify g f (y * size + x)
