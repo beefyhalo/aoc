2fb8d1185817f83f80a22b1f6cda5590af3efaca
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-19 17:05:03 -0500

    2015 day 10

---
+++ src/2015/10.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+
+import Data.List (group)
+import Data.Monoid (Endo (..))
+import Data.Semigroup (stimes)
+
+-- $setup
+-- >>> example = ["1", "11", "21", "1211", "111221"]
+
+main :: IO ()
+main = do
+  input <- readFile "input/2015/10.txt"
+  print $ solve 40 input
+  print $ solve 50 input
+
+solve :: Int -> String -> Int
+solve n = length . appEndo (stimes n (Endo step))
+
+-- >>> map step example
+-- ["11","21","1211","111221","312211"]
+step :: String -> String
+step = concatMap (\g -> show (length g) ++ [head g]) . group
