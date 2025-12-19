06f0edcbdbc450d0f5301d06935d55caacd39e93
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-19 14:14:21 -0500

    use bitvec

--- src/2015/06.hs
+++ src/2015/06.hs
@@ -5,20 +5,15 @@
 module Main (main) where
 
 import Control.Monad.ST (ST, runST)
-import Data.Bits (clearBit, complementBit, popCount, setBit)
+import Data.Bit (Bit (Bit), countBits, invertInPlace)
 import Data.Foldable (for_)
 import Data.List (stripPrefix)
 import Data.List.Split (splitOn)
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
-import Data.Word (Word64)
 
-size, rowWords :: Int
+size :: Int
 size = 1000
-rowWords = (size + 63) `div` 64
-
-index :: Int -> Int -> (Int, Int)
-index x y = (y * rowWords + x `div` 64, x `mod` 64)
 
 type Pos = (Int, Int)
 
@@ -48,19 +43,21 @@ parse s
 -- 4
 solve, partTwo :: [Op] -> Int
 solve ops = runST $ do
-  g <- MV.replicate (size * rowWords) 0
+  g <- MV.replicate (size * size) (Bit False)
   for_ ops (applyBit g)
-  V.sum . V.map popCount <$> V.freeze g
+  countBits <$> V.freeze g
 
-applyBit :: MV.MVector s Word64 -> Op -> ST s ()
+applyBit :: MV.MVector s Bit -> Op -> ST s ()
 applyBit g = \case
-  On p1 p2 -> opBitRange setBit p1 p2
-  Off p1 p2 -> opBitRange clearBit p1 p2
-  Toggle p1 p2 -> opBitRange complementBit p1 p2
+  On p1 p2 -> opBitRange (`MV.set` Bit True) p1 p2
+  Off p1 p2 -> opBitRange (`MV.set` Bit False) p1 p2
+  Toggle p1 p2 -> opBitRange invertInPlace p1 p2
   where
     opBitRange f (x1, y1) (x2, y2) =
-      for_ [y1 .. y2] $ \y -> for_ [x1 .. x2] $ \x ->
-        let (wi, bi) = index x y in MV.modify g (`f` bi) wi
+      for_ [y1 .. y2] $ \y -> do
+        let width = x2 - x1 + 1
+        let offset = y * size + x1
+        f $ MV.slice offset width g
 
 -- >>> partTwo [On (0,0) (999,999), Off (499,499) (500,500), Toggle (0,0) (999,999)]
 -- 2999996
@@ -76,5 +73,6 @@ applyBright g = \case
   Toggle p1 p2 -> opIntRange (+ 2) p1 p2
   where
     opIntRange f (x1, y1) (x2, y2) =
-      for_ [y1 .. y2] $ \y -> for_ [x1 .. x2] $ \x ->
-        MV.modify g f (y * size + x)
+      for_ [y1 .. y2] $ \y ->
+        for_ [x1 .. x2] $ \x ->
+          MV.modify g f (y * size + x)
