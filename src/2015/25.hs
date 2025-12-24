ecc6446befddf31c301156cb24cd03d94d0d7ada
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-24 15:13:51 -0500

    mod exponentiation

--- src/2015/25.hs
+++ src/2015/25.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Main (main) where
 
 import Data.Char (isDigit)
 import Data.List.Split (splitOn)
+import Data.Mod (Mod (..), (^%))
 
-startCode, multiplier, modulus :: Int
+-- The modulus as a type-level natura
+startCode, multiplier :: Mod 33554393
 startCode = 20151125
 multiplier = 252533
-modulus = 33554393
 
 -- $setup
 -- >>> input = "To continue, please consult the code grid in the manual.  Enter the code at row 2, column 1."
@@ -27,15 +29,13 @@ parse s = (r, c)
 -- >>> solve exampleRow exampleCol
 -- 31916031
 solve :: Int -> Int -> Int
-solve row col = codes !! seqIndex row col
+solve row col = fromIntegral . unMod $ startCode * multiplier ^% seqIndex row col
+
+-- solve row col = codes !! seqIndex row col
+--   where codes = iterate (\prev -> (prev * multiplier) `mod` modulus) startCode
 
 seqIndex :: Int -> Int -> Int
 seqIndex row col = diagStart + col - 1
   where
     n = row + col - 1 -- diagonal number
     diagStart = (n * (n - 1)) `div` 2 -- total codes before this diagonal
-
--- >>> take 5 codes
--- [20151125,31916031,18749137,16080970,21629792]
-codes :: [Int]
-codes = iterate (\prev -> (prev * multiplier) `mod` modulus) startCode
