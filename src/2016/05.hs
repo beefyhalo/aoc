c83697b3307cf40afcb29eee94c6029ee7954a1c
Kevin Horlick <beefyhalo@users.noreply.github.com>
2025-12-29 17:57:03 -0500

    2016 day 5

---
+++ src/2016/05.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
+import Crypto.Hash (Digest, MD5, hash)
+import Data.Bits (shiftR, (.&.))
+import qualified Data.ByteArray as BA
+import qualified Data.ByteString.Char8 as BC
+import Data.Foldable (find, fold, toList)
+import qualified Data.IntMap.Strict as M
+import Data.Word (Word8)
+
+main :: IO ()
+main = do
+  input <- BC.readFile "input/2016/05.txt"
+  putStrLn $ solve input
+  putStrLn $ partTwo input
+
+-- >>> solve "abc"
+-- >>> partTwo "abc"
+-- "18f47a30"
+-- "05ace8e3"
+solve, partTwo :: BC.ByteString -> String
+solve key =
+  take
+    8
+    [ toHex $ BA.index d 2
+    | n <- [0 ..],
+      let d = hash (key <> BC.pack (show n)),
+      hasFiveZeros d
+    ]
+
+toHex :: Word8 -> Char
+toHex n = "0123456789abcdef" !! fromIntegral n
+
+hasFiveZeros :: Digest MD5 -> Bool
+hasFiveZeros d = BA.index d 0 == 0 && BA.index d 1 == 0 && BA.index d 2 .&. 0xF0 == 0
+
+partTwo key = toList $ fold $ find ((> 7) . M.size) $ scanl step M.empty [0 ..]
+  where
+    step :: M.IntMap Char -> Int -> M.IntMap Char
+    step m n
+      | hasFiveZeros d && pos < 8 && M.notMember pos m = M.insert pos val m
+      | otherwise = m
+      where
+        d = hash (key <> BC.pack (show n))
+        pos = fromIntegral (BA.index d 2)
+        val = toHex (BA.index d 3 `shiftR` 4)
