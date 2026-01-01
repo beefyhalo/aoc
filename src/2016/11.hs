3b18f5cacfb4e7c9de90eac23b324ae19f1dcb36
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-01 12:18:12 -0500

    2016 day 11

---
+++ src/2016/11.hs
@@ -0,0 +1,15 @@
+import Control.Lens (ix, (&), (+~))
+
+main :: IO ()
+main = do
+  input <- parse <$> readFile "input/2016/11.txt"
+  print $ solve input
+  print $ solve (input & ix 0 +~ 4)
+
+parse :: String -> [Int]
+parse = map count . lines
+  where
+    count l = length [() | w : _ <- drop 4 (words l), w `elem` "gm"]
+
+solve :: [Int] -> Int
+solve xs = sum [if c == 0 then 0 else 2 * c - 1 | c <- init $ scanl (+) 0 xs]
