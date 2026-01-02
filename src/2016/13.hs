23d20ce40b6626ba1c66cb337d4d25d4b917351c
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-02 17:51:05 -0500

    2016 day 13

---
+++ src/2016/13.hs
@@ -0,0 +1,35 @@
+import Data.Bits (popCount)
+import Data.Maybe (fromMaybe)
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
+
+type Point = (Int, Int)
+
+main :: IO ()
+main = do
+  input <- read <$> readFile "input/2016/13.txt"
+  print $ solve input
+  print $ partTwo input
+
+solve, partTwo :: Int -> Int
+solve = fromMaybe 0 . fst . bfs (== (31, 39)) 1000 (1, 1)
+partTwo = Set.size . snd . bfs (const False) 50 (1, 1)
+
+isOpen :: Int -> Point -> Bool
+isOpen n (x, y) = even $ popCount $ x * x + 3 * x + 2 * x * y + y + y * y + n
+
+neighbors :: Point -> [Point]
+neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+bfs :: (Point -> Bool) -> Int -> Point -> Int -> (Maybe Int, Set.Set Point)
+bfs stop maxDist start n = go (Seq.singleton (start, 0)) (Set.singleton start)
+  where
+    go Seq.Empty seen = (Nothing, seen)
+    go ((here, dist) Seq.:<| rest) seen
+      | stop here = (Just dist, seen)
+      | dist >= maxDist = go rest seen
+      | otherwise =
+          let next = [p | p <- neighbors here, isOpen n p, p `Set.notMember` seen, p >= (0, 0)]
+              rest' = rest <> Seq.fromList [(p, dist + 1) | p <- next]
+              seen' = foldr Set.insert seen next
+           in go rest' seen'
