f8baf1a7c96031cfdac0f8e7936cf478eaa3db56
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-04 15:41:38 -0500

    use Sequence

--- src/2016/21.hs
+++ src/2016/21.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
+import Control.Lens (ix, (&), (.~))
 import Control.Monad (ap)
-import Data.List (elemIndex)
+import Data.Sequence (Seq, deleteAt, elemIndexL, index, insertAt)
+import qualified Data.Sequence as Seq
 
 data Instruction
   = SwapPos Int Int
@@ -22,8 +25,8 @@ data Instruction
 main :: IO ()
 main = do
   input <- map parse . lines <$> readFile "input/2016/21.txt"
-  putStrLn $ solve "abcdefgh" input
-  putStrLn $ partTwo "fbgdceah" input
+  print $ solve "abcdefgh" input
+  print $ partTwo "fbgdceah" input
 
 parse :: String -> Instruction
 parse line = case words line of
@@ -38,23 +41,23 @@ parse line = case words line of
 
 -- >>> solve "abcde" example
 -- >>> partTwo "decab" example
--- "decab"
--- "abcde"
-solve, partTwo :: String -> [Instruction] -> String
+-- fromList "decab"
+-- fromList "abcde"
+solve, partTwo :: Seq Char -> [Instruction] -> Seq Char
 solve = foldl' apply
 partTwo = foldr (ap apply . flip invert)
 
-apply :: String -> Instruction -> String
+apply :: Seq Char -> Instruction -> Seq Char
 apply s = \case
-  SwapPos x y -> [if i == x then s !! y else if i == y then s !! x else c | (i, c) <- zip [0 ..] s]
-  SwapLetter a b -> [if c == a then b else if c == b then a else c | c <- s]
-  RotateLeft n -> let k = n `mod` length s in drop k s ++ take k s
-  RotateRight n -> apply s (RotateLeft (-n))
-  RotatePos c -> let Just i = elemIndex c s in apply s (RotateRight $ 1 + i + i `div` 4)
-  ReversePos x y -> take x s ++ reverse (take (y - x + 1) (drop x s)) ++ drop (y + 1) s
-  MovePos x y -> let s' = take x s ++ drop (x + 1) s in take y s' ++ [s !! x] ++ drop y s'
+  SwapPos i j -> s & ix i .~ index s j & ix j .~ index s i
+  SwapLetter a b -> let Just i = elemIndexL a s; Just j = elemIndexL b s in s & ix i .~ b & ix j .~ a
+  RotateLeft n -> let k = n `mod` length s in Seq.drop k s <> Seq.take k s
+  RotateRight n -> s `apply` RotateLeft (-n)
+  RotatePos c -> let Just i = elemIndexL c s in s `apply` RotateRight (1 + i + i `div` 4)
+  ReversePos x y -> let (l, rest) = Seq.splitAt x s; (m, r) = Seq.splitAt (y - x + 1) rest in l <> Seq.reverse m <> r
+  MovePos i j -> insertAt j (index s i) (deleteAt i s)
 
-invert :: String -> Instruction -> Instruction
+invert :: Seq Char -> Instruction -> Instruction
 invert s = \case
   RotateLeft n -> RotateRight n
   RotateRight n -> RotateLeft n
