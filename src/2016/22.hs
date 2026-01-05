84d039d742d60d46be47c1c6ad1db9d93e4ea1d1
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-05 10:52:39 -0500

    2016 day 23

--- src/2016/22.hs
+++ src/2016/22.hs
@@ -8,7 +8,9 @@ import Data.Sequence (Seq (..))
 import qualified Data.Sequence as Seq
 import qualified Data.Set as Set
 
-data Node = Node {x, y, size, used, avail :: Int} deriving (Show, Eq)
+type Pos = (Int, Int)
+
+data Node = Node {pos :: Pos, size, used, avail :: Int} deriving (Show, Eq)
 
 -- $setup
 -- >>> input = "/dev/grid/node-x0-y0   10T    8T     2T   80%\n/dev/grid/node-x0-y1   11T    6T     5T   54%\n/dev/grid/node-x0-y2   32T   28T     4T   87%\n/dev/grid/node-x1-y0    9T    7T     2T   77%\n/dev/grid/node-x1-y1    8T    0T     8T    0%\n/dev/grid/node-x1-y2   11T    7T     4T   63%\n/dev/grid/node-x2-y0   10T    6T     4T   60%\n/dev/grid/node-x2-y1    9T    8T     1T   88%\n/dev/grid/node-x2-y2    9T    6T     3T   66%"
@@ -21,7 +23,7 @@ main = do
   print $ partTwo input
 
 parse :: String -> Node
-parse line = Node x y size used avail
+parse line = Node (x, y) size used avail
   where
     name : ints = words line
     [_, x, y] = read . drop 1 <$> splitOn "-" name
@@ -35,17 +37,15 @@ solve, partTwo :: [Node] -> Int
 solve ns = length [(a, b) | a <- ns, b <- ns, a /= b, used a > 0, used a <= avail b]
 partTwo ns = fromMaybe 0 distToGoal + 5 * (maxX - 1) + 1 -- Each move toward the goal uses 5 steps + the final swap.
   where
-    maxX = maximum $ map x ns
-    maxY = maximum $ map y ns
+    (maxX, maxY) = maximum $ map pos ns
 
     emptyNode = head [n | n <- ns, used n == 0]
-    startPos = (x emptyNode, y emptyNode)
     goal = (maxX - 1, 0)
-    walls = Set.fromList [(x n, y n) | n <- ns, used n > size emptyNode]
-    distToGoal = bfs startPos goal walls maxX maxY
+    walls = Set.fromList [pos n | n <- ns, used n > size emptyNode]
+    distToGoal = bfs (pos emptyNode) (maxX, maxY) goal walls
 
-bfs :: (Int, Int) -> (Int, Int) -> Set.Set (Int, Int) -> Int -> Int -> Maybe Int
-bfs start goal walls mx my = go (Seq.singleton (start, 0)) (Set.singleton start)
+bfs :: Pos -> Pos -> Pos -> Set.Set Pos -> Maybe Int
+bfs start (mx, my) goal walls = go (Seq.singleton (start, 0)) (Set.singleton start)
   where
     go Empty _ = Nothing
     go ((curr, dist) :<| rest) seen
---
+++ src/2016/23.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
+import Control.Applicative ((<|>))
+import Control.Lens
+import qualified Data.List.NonEmpty as NE
+import qualified Data.List.NonEmpty.Zipper as Z
+import Data.List.Split (wordsBy)
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+
+type Reg = Char
+
+type Regs = (Int, Int, Int, Int)
+
+access :: Reg -> Lens' Regs Int
+access = \case
+  'a' -> _1
+  'b' -> _2
+  'c' -> _3
+  'd' -> _4
+
+data Arg = R Reg | V Int deriving (Show)
+
+data Ins
+  = Cpy Arg Arg
+  | Inc Reg
+  | Dec Reg
+  | Jnz Arg Arg
+  | Tgl Arg
+  deriving (Show)
+
+-- $setup
+-- >>> input = "cpy 2 a\ntgl a\ntgl a\ntgl a\ncpy 1 a\ndec a\ndec a"
+-- >>> example = parse input
+
+main :: IO ()
+main = do
+  input <- parse <$> readFile "input/2016/23.txt"
+  print $ solve input (7, 0, 0, 0) ^. _1
+  print $ solve input (12, 0, 0, 0) ^. _1
+
+parse :: String -> Z.Zipper Ins
+parse = Z.fromNonEmpty . NE.fromList . map parseLine . lines
+  where
+    parseArg s = maybe (R (head s)) V (readMaybe s)
+    parseLine s = case wordsBy (`elem` " ,+") s of
+      ["cpy", x, [y]] -> Cpy (parseArg x) (R y)
+      ["inc", [x]] -> Inc x
+      ["dec", [x]] -> Dec x
+      ["jnz", x, y] -> Jnz (parseArg x) (parseArg y)
+      ["tgl", x] -> Tgl (parseArg x)
+
+-- >>> solve example (7, 0, 0, 0)
+-- (3,0,0,0)
+solve :: Z.Zipper Ins -> Regs -> Regs
+solve z rs = maybe rs (uncurry solve) $ step z rs
+
+get :: Regs -> Arg -> Int
+get rs = \case
+  V n -> n
+  R r -> rs ^. access r
+
+toggle :: Ins -> Ins
+toggle = \case
+  Inc r -> Dec r
+  Dec r -> Inc r
+  Tgl (R r) -> Inc r
+  Jnz x y -> Cpy x y
+  Cpy x y -> Jnz x y
+
+ixRel :: Int -> Traversal' (Z.Zipper a) a
+ixRel n f z = case moveOffset n z of
+  Nothing -> pure z
+  Just focused ->
+    let back = moveOffset (-n)
+     in fromMaybe z . back . (`Z.replace` focused) <$> f (Z.current focused)
+
+step :: Z.Zipper Ins -> Regs -> Maybe (Z.Zipper Ins, Regs)
+step z regs =
+  optimize z regs <|> case Z.current z of
+    Cpy src (R dst) -> (,regs & access dst .~ get regs src) <$> Z.right z
+    Inc r -> (,regs & access r +~ 1) <$> Z.right z
+    Dec r -> (,regs & access r -~ 1) <$> Z.right z
+    Tgl arg -> (,regs) <$> Z.right (z & ixRel (get regs arg) %~ toggle)
+    Jnz x y -> (,regs) <$> moveOffset (if get regs x /= 0 then get regs y else 1) z
+
+-- detect a += b * d pattern
+-- Pattern: inc a, dec c, jnz c -2, dec d, jnz d -5
+optimize :: Z.Zipper Ins -> Regs -> Maybe (Z.Zipper Ins, Regs)
+optimize z rs = case Z.current z : take 5 (Z.rights z) of
+  [Cpy (R b) (R c), Inc a, Dec c', Jnz (R c'') (V (-2)), Dec d, Jnz (R d') (V (-5))]
+    | c == c' && c == c'' && d == d' ->
+        let rs' =
+              rs
+                & access a +~ (rs ^. access b) * (rs ^. access d)
+                & access c .~ 0
+                & access d .~ 0
+         in (,rs') <$> Z.rightN 6 z
+  _ -> Nothing
+
+moveOffset :: Int -> Z.Zipper a -> Maybe (Z.Zipper a)
+moveOffset n
+  | n > 0 = Z.rightN (fromIntegral n)
+  | n < 0 = Z.leftN (fromIntegral (abs n))
+  | otherwise = Just
