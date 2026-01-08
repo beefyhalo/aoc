83a46dbac288701fe428f94d4ee5222e8a2edc29
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-07 21:53:49 -0500

    clean

--- src/2017/07.hs
+++ src/2017/07.hs
@@ -1,8 +1,10 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
 
+import Data.Either (fromLeft)
 import Data.List (group, sort, sortOn)
+import Data.List.Split (wordsBy)
 import qualified Data.Map as M
-import Data.Maybe (fromJust)
 import Data.Tree (Tree (Node, rootLabel), unfoldTree)
 
 type Prog = (Int, [String])
@@ -17,29 +19,25 @@ main = do
 parse :: String -> (String, Prog)
 parse s = (name, (read weight, drop 1 children))
   where
-    name : weight : children = words $ filter (/= ',') s
+    name : weight : children = wordsBy (`elem` ", ") s
 
 solve :: M.Map String Prog -> String
 solve m = head [n | n <- M.keys m, n `notElem` children]
   where
     children = concatMap snd (M.elems m)
 
-partTwo :: String -> M.Map String (Int, [String]) -> Either Int Int
-partTwo root input = findBad tree
+partTwo :: String -> M.Map String Prog -> Int
+partTwo root input = fromLeft 0 $ findBad tree
   where
     tree = unfoldTree (\name -> let (w, children) = input M.! name in ((name, w), children)) root
 
 findBad :: Tree (String, Int) -> Either Int Int
 findBad (Node (_, weight) children) = do
   childWeights <- traverse findBad children
-
-  let grouped = sortOn length $ group $ sort childWeights
-  if length grouped <= 1
-    then Right (weight + sum childWeights) -- Subtree is balanced
-    else
-      -- We found the level with the imbalance
-      let [wrongWeight : _, rightWeight : _] = grouped
-          -- Find the child node that had the wrong weight
-          badChildIndex = fromJust $ lookup wrongWeight (zip childWeights [0 ..])
-          badChildWeight = snd $ rootLabel (children !! badChildIndex)
-       in Left (badChildWeight + rightWeight - wrongWeight)
+  case sortOn length (group $ sort childWeights) of
+    [[wrongWeight], rightWeight : _] -> Left (diff + badWeight)
+      where
+        diff = rightWeight - wrongWeight
+        Just badIndex = lookup wrongWeight (zip childWeights [0 ..])
+        (_, badWeight) = rootLabel (children !! badIndex)
+    _ -> Right (weight + sum childWeights) -- Subtree is balanced
