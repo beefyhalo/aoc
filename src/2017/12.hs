f3d0482238dd47ed9d9ef547689f95b4d72a5e61
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-10 16:14:13 -0500

    2017 day 12

---
+++ src/2017/12.hs
@@ -0,0 +1,43 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
+import qualified Data.IntMap.Strict as IM
+import qualified Data.IntSet as IS
+import Data.List.Split (splitOn)
+
+type Graph = IM.IntMap [Int]
+
+-- $setup
+-- >>> input = "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5"
+-- >>> example = IM.fromList (map parse (lines input))
+
+main :: IO ()
+main = do
+  g <- IM.fromList . map parse . lines <$> readFile "input/2017/12.txt"
+  print $ solve g
+  print $ partTwo g
+
+parse :: String -> (Int, [Int])
+parse s = (read a, read <$> splitOn ", " b)
+  where
+    [a, b] = splitOn " <-> " s
+
+-- >>> solve example
+-- >>> partTwo example
+-- 6
+-- 2
+solve, partTwo :: Graph -> Int
+solve g = IS.size $ dfs g 0
+partTwo g = go $ IM.keysSet g
+  where
+    go r
+      | IS.null r = 0
+      | otherwise =
+          let c = dfs g (IS.findMin r)
+           in 1 + go (r IS.\\ c)
+
+dfs :: Graph -> Int -> IS.IntSet
+dfs g = go IS.empty
+  where
+    go seen node
+      | IS.member node seen = seen
+      | otherwise = foldl' go (IS.insert node seen) (IM.findWithDefault [] node g)
