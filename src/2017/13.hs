619c1dd245d2ef657848140dca47d70e66be7234
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-01-11 15:55:48 -0500

    2017 day 13

---
+++ src/2017/13.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
+import Data.List.Split (splitOn)
+import Data.List (sortOn)
+
+-- $setup
+-- >>> input = "0: 3\n1: 2\n4: 4\n6: 4"
+-- >>> example = map parse (lines input)
+
+main :: IO ()
+main = do
+  input <- map parse . lines <$> readFile "input/2017/13.txt"
+  print $ solve input
+  print $ partTwo input
+
+parse :: String -> (Int, Int)
+parse s = (read d, read r)
+  where
+    [d, r] = splitOn ":" s
+
+-- >>> solve example
+-- >>> partTwo example
+-- 24
+-- 10
+solve, partTwo :: [(Int, Int)] -> Int
+solve fw = sum [d * r | (d, r) <- fw, caught 0 (d, r)]
+partTwo fw = head [delay | delay <- [0 ..], all (not . caught delay) $ sortOn snd fw]
+
+caught :: Int -> (Int, Int) -> Bool
+caught delay (d, r) = (d + delay) `mod` period == 0
+  where
+    period = 2 * (r - 1)
