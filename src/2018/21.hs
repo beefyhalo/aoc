e72cf6bfd09cdca521d092378557f154c5e029dd
Kevin Horlick <570857+beefyhalo@users.noreply.github.com>
2026-02-12 15:07:11 -0500

    2018 day 21

---
+++ src/2018/21.hs
@@ -0,0 +1,43 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
+import Data.Bits ((.&.), (.|.))
+import qualified Data.IntSet as S
+import Data.Tuple.Extra (snd3)
+
+main :: IO ()
+main = do
+  input <- parse <$> readFile "input/2018/21.txt"
+  print $ solve input
+
+parse :: String -> Int
+parse ls = maximum [read n | line <- lines ls, ["seti", n, _, _] <- [words line]]
+
+-- >>> solve 123
+-- (5692748,4081319)
+solve :: Int -> (Int, Int)
+solve n = (head stream, lastUnique stream)
+  where
+    _ : stream = iterate (next n) 0
+
+-- mimic this assembly loop
+-- loop:
+--   acc = mix(acc, r3 & 255)
+--   if r3 < 256: break
+--   r3 = r3 / 256
+--   goto loop
+next :: Int -> Int -> Int
+next seed acc =
+  snd $
+    until
+      ((== 0) . fst)
+      (\(r3, r4) -> (r3 `div` 256, (((r4 + (r3 .&. 255)) .&. 16777215) * 65899) .&. 16777215))
+      (acc .|. 65536, seed)
+
+lastUnique :: [Int] -> Int
+lastUnique xs =
+  snd3 $
+    until
+      (\(seen, _, y : _) -> S.member y seen)
+      (\(seen, _, y : ys) -> (S.insert y seen, y, ys))
+      (S.empty, 0, xs)
